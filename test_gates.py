da2f3269b9682ea5825ffc4c09c00893926db997
Gian Gisin <gian.gisin@gmail.com>
2023-08-13 20:46:13 +0200

    or/and gates test cases

--- test_gates.py
+++ test_gates.py
@@ -1,2 +1,38 @@
 import unittest
 from gates.gates import *
+
+class Gates_test(unittest.TestCase):
+    def test_and(self):
+        gate = And_Gate()
+
+        gate.A.trigger(False)
+        gate.B.trigger(False)
+        self.assertEqual(gate.Q.state, False)
+
+        gate.A.trigger(True)
+        gate.B.trigger(False)
+        self.assertEqual(gate.Q.state, False)
+
+        gate.A.trigger(True)
+        gate.B.trigger(True)
+        self.assertEqual(gate.Q.state, True)
+
+    def test_or(self):
+        gate = Or_Gate()
+
+        gate.A.trigger(False)
+        gate.B.trigger(False)
+        self.assertEqual(gate.Q.state, False)
+
+        gate.A.trigger(True)
+        gate.B.trigger(False)
+        self.assertEqual(gate.Q.state, True)
+
+        gate.A.trigger(True)
+        gate.B.trigger(True)
+        self.assertEqual(gate.Q.state, True)
+
+        
+
+
+unittest.main()
